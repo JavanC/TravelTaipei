e3bddb3819828f43f03e066194cfe080422edcbc
Javan <fu6gjp4@gmail.com>
2019-10-25 16:37:14 +0800

    add: Travel Taipei Project setup

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TravelTaipei' do
+  use_frameworks!
+  pod 'Alamofire', '~> 4.8.2'
+  pod 'RxSwift', '~> 5.0.0'
+  pod 'Kingfisher', '~> 5.7.0'
+  pod 'SnapKit', '~> 5.0.0'
+
+  target 'TravelTaipeiTests' do
+    inherit! :search_paths
+  end
+end
---
+++ TravelTaipei/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelTaipei
+//
+//  Created by Javan on 2019/10/25.
+//  Copyright © 2019 Javan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
